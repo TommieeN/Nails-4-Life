4473f05f5689c83f73a8946b34a68b3f228a33cf
TommyNguyen <timsnguyen@hotmail.com>
2024-02-10 17:24:01 -0800

    main section finish for mobile

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -29,13 +29,12 @@ const MainSection = () => {
           alt="woman with posing with her back turned with no shirt with flowers in her back pocket and hands in the air"
         />
         <div className="main-container">
-          <h2 className="main-container__header">Nails</h2>
+          <h2 className="main-container__header">Waxing</h2>
           <p className="main-container__text">
-            Indulge in the ultimate nail care experience at Nails 4 Life. Our
-            skilled technicians offer a range of services including manicures,
-            pedicures, and nail enhancements. Relax in our comfortable
-            environment and treat yourself to beautiful nails that make a
-            statement. Book your appointment today!
+            Discover smooth, radiant skin with our expert waxing services. From
+            brows to legs, we use gentle techniques and quality products for
+            lasting results. Experience comfort and hygiene at Nails 4 Life. Say
+            hello to silky skin today!
           </p>
           <button className="main-container__button">Learn More</button>
         </div>
