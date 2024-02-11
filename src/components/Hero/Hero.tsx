b08f745e9ec0c99e2787476ff6294621b5e07dc3
TommyNguyen <timsnguyen@hotmail.com>
2024-02-10 16:38:38 -0800

    add alt text for hero image

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,7 +3,11 @@ import hero from "../../assets/hero-nail-img.jpg";
 const Hero = () => {
   return (
     <section className="hero">
-      <img className="hero-banner" src={hero} />
+      <img
+        className="hero-banner"
+        src={hero}
+        alt="Young woman with white nail polish posing for a photo with hand on shoulder"
+      />
       <h1 className="hero-header">Nails 4 Life</h1>
       <div className="hero-text">
         <p>Elevate Your Style</p>
@@ -11,7 +15,9 @@ const Hero = () => {
       </div>
       <div className="hero-button">
         <button className="hero-button__primary">Book Now!</button>
-        <button className="hero-button__primary hero-button__primary-services">Services</button>
+        <button className="hero-button__primary hero-button__primary-services">
+          Learn More
+        </button>
       </div>
     </section>
   );
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 import Hero from "../../components/Hero/Hero";
+import MainSection from "../../components/MainSection/MainSection";
 import "./HomePage.scss";
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
+      <MainSection />
     </main>
   )
 }
