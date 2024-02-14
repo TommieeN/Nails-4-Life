8217b5b698a2da78319bb9b24328eb7d03c56f05
TommyNguyen <timsnguyen@hotmail.com>
2024-02-13 21:18:22 -0800

    add testemonial data and cards

--- src/components/Testemonials/Testemonials.tsx
+++ src/components/Testemonials/Testemonials.tsx
@@ -1,12 +1,29 @@
 import ImageCarousel from "../ImageCarousel/ImageCarousel";
+import { testemonials } from "../../constants";
 import "./Testemonials.scss";
-
+import { RxAvatar } from "react-icons/rx";
 const Testemonials = () => {
   return (
-    <div>
+    <section className="testemonial-container">
       <ImageCarousel />
-    </div>
-  )
-}
+      {testemonials.map((test, index) => {
+        return (
+          <div className="testemonial-container__cards" key={index}>
+            <div className="testemonial-container__user">
+              <RxAvatar size={50} />
+              <div className="testemonial-container__name">
+                <p>{test.name}</p>
+                <p>{test.userName}</p>
+              </div>
+            </div>
+            <div>
+              <p>{test.text}</p>
+              </div>
+          </div>
+        );
+      })}
+    </section>
+  );
+};
 
-export default Testemonials
+export default Testemonials;
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,9 +1,27 @@
-import nailImage1 from '../assets/nail-assets/nail-test.jpg'
-import nailImage2 from '../assets/nail-assets/nail-test2.jpg'
-import nailImage3 from '../assets/nail-assets/nail-test3.jpg'
-import nailImage4 from '../assets/nail-assets/nail-test4.jpg'
+import nailImage1 from "../assets/nail-assets/nail-test.jpg";
+import nailImage2 from "../assets/nail-assets/nail-test2.jpg";
+import nailImage3 from "../assets/nail-assets/nail-test3.jpg";
+import nailImage4 from "../assets/nail-assets/nail-test4.jpg";
 
+export const images = [nailImage1, nailImage2, nailImage3, nailImage4];
 
-export const images = [
-    nailImage1, nailImage2, nailImage3, nailImage4
-]
+export const testemonials = [
+  {
+    avatar: "https://placehold.co/600x400/000000/FFFFFF/png",
+    name: "Eva Elle",
+    userName: "@evaelle",
+    text: "Thank you for building such an empowering tool, especially for designers! The site went from Figma to Framer in less than a week!",
+  },
+  {
+    avatar: "https://placehold.co/600x400/000000/FFFFFF/png",
+    name: "Guy Mccoy",
+    userName: "@mccoy",
+    text: "Playing around with @framer while building a landing page for a side project. I’m terrible at animations, but they make it so easy!",
+  },
+  {
+    avatar: "https://placehold.co/600x400/000000/FFFFFF/png",
+    name: "Kayla Ray",
+    userName: "@kayray",
+    text: "I’ve built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
+  },
+];
