2b105e9fcf1c83c4dd35b5d402e6781cafdf9785
TommyNguyen <timsnguyen@hotmail.com>
2024-04-18 16:14:44 -0700

    finish hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,16 +8,17 @@ const Hero = () => {
         src={hero}
         alt="Young woman with white nail polish posing for a photo with hand on shoulder"
       />
-      <div>
+      <div className="hero-desktop-container">
         <div className="hero-tablet-text">
           <h1 className="hero-header">Nails 4 Life</h1>
           <div className="hero-text">
-            <p>Elevate Your Style,</p>
-            <p>Enhance Your Beauty</p>
+            <p className="hero-desktop-text">Elevate Your Style,</p>
+            <p className="hero-desktop-text">Enhance Your Beauty</p>
           </div>
         </div>
         <div className="hero-button">
           <a
+          className="hero-button__primary"
             target="_blank"
             href="https://www.dashbooking.com/salon/nails-4-life"
           >
