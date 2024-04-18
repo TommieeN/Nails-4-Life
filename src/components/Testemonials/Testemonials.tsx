087d5c8ec3ab418e5723001993447a2d961c4e15
TommyNguyen <timsnguyen@hotmail.com>
2024-04-18 15:45:42 -0700

    finish carousel section for tablet

--- src/components/Testemonials/Testemonials.tsx
+++ src/components/Testemonials/Testemonials.tsx
@@ -1,31 +1,11 @@
 import ImageCarousel from "../ImageCarousel/ImageCarousel";
-import { testemonials } from "../../constants";
 import "./Testemonials.scss";
-import { RxAvatar } from "react-icons/rx";
 const Testemonials = () => {
   return (
-    <>
+    <section className="carousel-tablet-container">
+      <h2>Gallery</h2>
       <ImageCarousel />
-      
-      {/* <section className="testemonial">
-        {testemonials.map((test, index) => {
-          return (
-            <div className="testemonial__cards" key={index}>
-              <div className="testemonial__user">
-                <RxAvatar size={50} />
-                <div className="testemonial__name-container">
-                  <p className="testemonial__name">{test.name}</p>
-                  <p>{test.userName}</p>
-                </div>
-              </div>
-              <div>
-                <p>{test.text}</p>
-              </div>
-            </div>
-          );
-        })}
-      </section> */}
-    </>
+    </section>
   );
 };
 
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -5,27 +5,6 @@ import nailImage4 from "../assets/nail-assets/nail-test4.jpg";
 
 export const images = [nailImage1, nailImage2, nailImage3, nailImage4];
 
-export const testemonials = [
-  {
-    avatar: "https://placehold.co/600x400/000000/FFFFFF/png",
-    name: "Eva Elle",
-    userName: "@evaelle",
-    text: "Thank you for building such an empowering tool, especially for designers! The site went from Figma to Framer in less than a week!",
-  },
-  {
-    avatar: "https://placehold.co/600x400/000000/FFFFFF/png",
-    name: "Guy Mccoy",
-    userName: "@mccoy",
-    text: "Playing around with @framer while building a landing page for a side project. I’m terrible at animations, but they make it so easy!",
-  },
-  {
-    avatar: "https://placehold.co/600x400/000000/FFFFFF/png",
-    name: "Kayla Ray",
-    userName: "@kayray",
-    text: "I’ve built pretty handy sites powered by Craft or WordPress in the past, but seeing @framer tackle CMS stuff so effortlessly is mind-boggling",
-  },
-];
-
 export const days = [
     { name: "Mon", hours: "10:00AM - 7:00PM" },
     { name: "Tue", hours: "10:00AM - 7:00PM" },
