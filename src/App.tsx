728eb3f3415d32c96890d54cc19468cbd1ebb7d9
TommyNguyen <timsnguyen@hotmail.com>
2024-04-18 17:41:02 -0700

    change logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import HomePage from "./pages/HomePage/HomePage";
-import NavBar from "./components/NavBar/NavBar";
-import Services from "./components/Services/Services";
 
 function App() {
   return (
     <>
-    {/* <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/services" element={<Services />} />
-      </Routes>
-    </BrowserRouter> */}
     <HomePage />
     </>
   );
