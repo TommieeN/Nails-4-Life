db2a0218c7d5575de5e9890ea6eec3f40fd5f9a6
TommyNguyen <timsnguyen@hotmail.com>
2024-04-19 21:29:19 -0700

    new widget for photos

--- src/components/Testemonials/Testemonials.tsx
+++ src/components/Testemonials/Testemonials.tsx
@@ -6,7 +6,8 @@ const Testemonials = () => {
       <h2 id="Reviews" className="carousel-tablet-container__heading">Reviews</h2>
       <div className='sk-ww-google-reviews' data-embed-id='25400597'></div>
       <h2 className="carousel-tablet-container__gallery">Gallery</h2>
-      <ImageCarousel />
+      <behold-widget feed-id="euXvxC7dm501c1MThVhP"></behold-widget>
+      {/* <ImageCarousel /> */}
     </section>
   );
 };
