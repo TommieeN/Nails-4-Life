a9816e4865e79ebdc8e33109048033d4459c5892
TommyNguyen <timsnguyen@hotmail.com>
2024-04-19 17:54:50 -0700

    add google reviews widget

--- src/components/Testemonials/Testemonials.tsx
+++ src/components/Testemonials/Testemonials.tsx
@@ -5,6 +5,7 @@ const Testemonials = () => {
     <section className="carousel-tablet-container">
       <h2 className="carousel-tablet-container__heading">Gallery</h2>
       <ImageCarousel />
+      <div className="elfsight-app-2334fd14-f9d7-4fa4-94e2-12cabb771548" data-elfsight-app-lazy></div>
     </section>
   );
 };
