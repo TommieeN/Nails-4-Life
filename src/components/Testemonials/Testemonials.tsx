528e76c62e959ac4409673fdb0258f459fc8c6ca
TommyNguyen <timsnguyen@hotmail.com>
2024-04-19 21:35:07 -0700

    fix typescript error

--- src/components/Testemonials/Testemonials.tsx
+++ src/components/Testemonials/Testemonials.tsx
@@ -1,11 +1,12 @@
-import "./Testemonials.scss";
+import React from "react";
+
 const Testemonials = () => {
   return (
     <section className="carousel-tablet-container">
       <h2 id="Reviews" className="carousel-tablet-container__heading">Reviews</h2>
       <div className='sk-ww-google-reviews' data-embed-id='25400597'></div>
       <h2 className="carousel-tablet-container__gallery">Gallery</h2>
-      <behold-widget feed-id="euXvxC7dm501c1MThVhP"></behold-widget>
+      {(React.createElement as any)('behold-widget', { 'feed-id': 'euXvxC7dm501c1MThVhP' })}
     </section>
   );
 };
