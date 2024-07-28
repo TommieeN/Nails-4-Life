cdc721ed3faf5aac13283c3e612819d82e6ae457
TommyNguyen <timsnguyen@hotmail.com>
2024-07-27 18:41:28 -0700

    add new hero image

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,10 +1,3 @@
-import nailImage1 from "../assets/nail-assets/nail-test.jpg";
-import nailImage2 from "../assets/nail-assets/nail-test2.jpg";
-import nailImage3 from "../assets/nail-assets/nail-test3.jpg";
-import nailImage4 from "../assets/nail-assets/nail-test4.jpg";
-
-export const images = [nailImage1, nailImage2, nailImage3, nailImage4];
-
 export const days = [
     { name: "Mon", hours: "10:00AM - 7:00PM" },
     { name: "Tue", hours: "10:00AM - 7:00PM" },
