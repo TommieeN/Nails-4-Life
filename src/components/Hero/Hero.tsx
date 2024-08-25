6c72fad48f770f65aec6e44b53803815a899b2f6
TommyNguyen <timsnguyen@hotmail.com>
2024-08-24 19:48:50 -0700

    hero section revamp mobile

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,39 +1,66 @@
 import "./Hero.scss";
-import hero from "../../assets/nail-assets/hero-nail-salon.webp";
+import hero from "../../assets/nail-assets/bg-hero-lv.jpg";
+import { motion } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
+
 const Hero = () => {
   return (
-    <section className="hero" id="Home">
-      <img
-        className="hero-banner"
+    <motion.section
+      className="hero"
+      id="Home"
+      variants={container}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.img
+        className="hero__banner"
         src={hero}
         alt="Picture of the brand new nail salon with all new products and pedicure chairs"
+        variants={item}
       />
-      <div className="hero-desktop-container">
-        <div className="hero-tablet-text">
-          <h1 className="hero-header">Nails 4 Life</h1>
-          <div className="hero-text">
-            <p className="hero-desktop-text">Elevate Your Style</p>
-            <p className="hero-desktop-text">Enhance Your Beauty</p>
-            <a
-              target="_blank"
-              rel="noopener noreferrer" 
-              href="https://maps.app.goo.gl/yzNNXttg5CTqexHN8"
-              className="hero-desktop-text"
-            >
-              Located in Surrey, B.C, 15325 56 Ave #104
-            </a>
+      <div className="hero__container">
+        <div className="hero__text-container">
+          <motion.h1 className="hero__header" variants={item}>
+            Nails 4 Life
+          </motion.h1>
+          <div className="hero__info-container">
+            <motion.p className="hero__text" variants={item}>
+              Open 7 days a week
+            </motion.p>
+            <motion.p className="hero__text" variants={item}>
+              Senior 10% Discount Every Day
+            </motion.p>
           </div>
         </div>
-        <div className="hero-button">
+        <div className="hero__button">
           <a
-            className="hero-button__primary"
+            className="hero__button-primary"
             target="_blank"
             href="https://www.dashbooking.com/salon/nails-4-life"
           >
             Book Now!
           </a>
           <a
-            className="hero-button__primary"
+            className="hero__button-primary"
             target="_blank"
             href="https://www.dashbooking.com/salon/nails-4-life"
           >
@@ -41,7 +68,7 @@ const Hero = () => {
           </a>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
