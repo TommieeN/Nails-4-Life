d72823779aa2dc79f0f23128372bbf51455f0bc9
TommyNguyen <timsnguyen@hotmail.com>
2024-09-08 17:35:47 -0700

    add lazy loading to hero and font-swap

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -10,6 +10,7 @@ const Hero: React.FC = () => {
         className="hero__banner"
         src={hero}
         alt="Picture of a cart filled with nail supplies"
+        loading="lazy"
       />
       <div className="hero__container">
         <motion.div
